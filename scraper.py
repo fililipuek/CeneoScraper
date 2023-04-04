328e6c87e3794b767601c051908bf62dca4915f4
fililipuek <9wf3n5y87676h@gmail.com>
2023-04-04 17:50:23 +0200

    input: restore input prompt

--- scraper.py
+++ scraper.py
@@ -3,8 +3,7 @@ import json
 import os
 from bs4 import BeautifulSoup
 
-# product_id = input("Enter the product ID: ")
-product_id = "129901214"
+product_id = input("Enter the product ID: ")
 url = f"https://www.ceneo.pl/{product_id}#tab=reviews"
 response = requests.get(url)
 first = True
